3285ac22a80f08f9d507282a3ab0e94513d9e45b
ahrimy <dlsdjdkfla@gmail.com>
2021-03-25 02:02:30 +0900

    feat: PostThumbnail 클래스 생성
    - postId, image

---
+++ Travelog/Travelog/PostThumbnail.swift
@@ -0,0 +1,20 @@
+//
+//  PostThumbnail.swift
+//  Travelog
+//
+//  Created by Ahrim Yang on 2021/03/25.
+//
+
+import Foundation
+
+class PostThumbnail {
+    
+    init(postId: String , image: String){
+        self.postId = postId
+        self.image = image
+    }
+    
+    let postId : String
+    let image: String
+    
+}
