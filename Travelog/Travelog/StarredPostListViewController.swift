8aec3425099404f75f50d0c83537f4c7d37644e5
KangYena <62419355+KangYena@users.noreply.github.com>
2021-04-07 03:57:00 +0900

    feat : 북마크 페이지 리스트 기능 추가중, test용 json파일 및 이미지 asset 추가

--- Travelog/Travelog/StarredPostListViewController.swift
+++ Travelog/Travelog/StarredPostListViewController.swift
@@ -9,17 +9,77 @@ import UIKit
 
 class StarredPostListViewController: UIViewController {
     
-    @IBOutlet weak var LocationLabel: UILabel!
+    let sections = Bundle.main.decode([Section].self, from: "StarredPostList.json")
+    var StarredPostListCollectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, StarredPostList>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        /*
-        let LabelTapGesture = UITapGestureRecognizer(target: self, action: #selector(LocationLabel.addGestureRecognizer(_:)))
-        LocationLabel.addGestureRecognizer(LabelTapGesture)
-        */
+        
+        StarredPostListCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
+        StarredPostListCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        StarredPostListCollectionView.backgroundColor = .systemBackground
+        view.addSubview(StarredPostListCollectionView)
+        
+        StarredPostListCollectionView.register(StarredPostListCell.self, forCellWithReuseIdentifier: StarredPostListCell.reuseIdentifier)
+        
+        createDataSource()
+        reloadData()
     }
     
+    func configure<T: SelfConfiguringCell>(_ cellType: T.Type, with starredpostlist: StarredPostList, for indexPath: IndexPath) -> T {
+        guard let cell = StarredPostListCollectionView.dequeueReusableCell(withReuseIdentifier: cellType.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Unable to dequeue \(cellType)")
+        }
+        cell.configure(with: starredpostlist)
+        
+        return cell
+    }
+    
+    func createDataSource(){
+        dataSource = UICollectionViewDiffableDataSource<Section, StarredPostList>(collectionView: StarredPostListCollectionView){ StarredPostListCollectionView, indexPath, starredpostlist in switch self.sections[indexPath.section].type {
+        default: return self.configure(StarredPostListCell.self, with: starredpostlist, for: indexPath)}
+        }
+    }
+    
+    func reloadData(){
+        var snapshot = NSDiffableDataSourceSnapshot<Section, StarredPostList>()
+        snapshot.appendSections(sections)
+        
+        for section in sections {
+            snapshot.appendItems(section.items, toSection: section)
+        }
+        
+        dataSource?.apply(snapshot)
+    }
+    
+    func createCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in let section = self.sections[sectionIndex]
+            switch section.type {
+            default:
+                return self.createSection(using: section)
+            }
+        }
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+        layout.configuration = config
+        return layout
+    }
+    
+    func createSection(using section: Section) -> NSCollectionLayoutSection{
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        
+        let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let layoutGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(340))
+        
+        let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: layoutGroupSize, subitems: [layoutItem])
+        
+        let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
+        
+        return layoutSection
+    }
 
     /*
     // MARK: - Navigation
