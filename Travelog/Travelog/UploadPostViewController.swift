d975b96a3a068c2c360404e352cd21a33eb83e26
ahrimy <dlsdjdkfla@gmail.com>
2021-04-07 02:01:31 +0900

    feat: upload 화면에서 image 불러오기
    - imageview tab 하면 image 불러올 수 있음
    - 여러장 불러올 수 있도록 수정 필요

--- Travelog/Travelog/UploadPostViewController.swift
+++ Travelog/Travelog/UploadPostViewController.swift
@@ -2,36 +2,83 @@
 //  UploadPostViewController.swift
 //  Travelog
 //
-//  Created by Ahrim Yang on 2021/03/30.
+//  Created by Ahrim Yang on 2021/04/06.
 //
 
 import UIKit
+import PhotosUI
 
-class UploadPostViewController: UIViewController {
+class UploadPostViewController: UIViewController,PHPickerViewControllerDelegate {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var selectedPhoto: UIImageView!{
+        didSet {
+            selectedPhoto.isUserInteractionEnabled = true
+        }
+    }
+    
+    @IBOutlet weak var publicPrivateSegmentedControl: UISegmentedControl!{
+        didSet {
+            publicPrivateSegmentedControl.setTitleTextAttributes([.foregroundColor : UIColor.white], for: .selected)
+            publicPrivateSegmentedControl.setTitleTextAttributes([.foregroundColor : UIColor(red: 0.85, green: 0.72, blue: 0.76, alpha: 1.00)], for: .normal)
+        }
+    }
+    
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.tabBarController?.tabBar.isHidden = true
-        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor.white], for: .selected)
-        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor(red: 0.85, green: 0.72, blue: 0.76, alpha: 1.00)], for: .normal)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         self.tabBarController?.tabBar.isHidden = false
-        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor :        UIColor(red: 0.31, green: 0.16, blue: 0.36, alpha: 1.00)], for: .normal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    
+    @IBAction func didTapImageView(_ sender: UITapGestureRecognizer) {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .any(of: [.images, .videos])
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
     }
-    */
+    
+    // MARK: - Methods
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        let itemProvider = results.first?.itemProvider
+        if let itemProvider = itemProvider,
+           itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                DispatchQueue.main.async {
+                    self.selectedPhoto.image = image as? UIImage
+                }
+            }
+        } else {
+            // TODO: Handle empty results or item provider not being able load UIImage
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
