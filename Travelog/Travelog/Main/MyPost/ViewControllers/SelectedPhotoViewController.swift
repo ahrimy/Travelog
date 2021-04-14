fb88433fce2e42bd001bd51ec3df77a8f4a146da
ahrimy <dlsdjdkfla@gmail.com>
2021-04-14 23:18:37 +0900

    feat: 사진 최대 10장 선택할 수 있도록 설정

--- Travelog/Travelog/Main/MyPost/ViewControllers/SelectedPhotoViewController.swift
+++ Travelog/Travelog/Main/MyPost/ViewControllers/SelectedPhotoViewController.swift
@@ -10,7 +10,9 @@ import PhotosUI
 
 class SelectedPhotoViewController: UIViewController ,PHPickerViewControllerDelegate, UICollectionViewDelegate, UICollectionViewDataSource{
 
-    var selectedPhotos:[UIImage] = [];
+    var selectedPhotos:[UIImage] = []
+    var itemProviders:[NSItemProvider] = []
+    var iterator:IndexingIterator<[NSItemProvider]>?
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -24,6 +26,7 @@ class SelectedPhotoViewController: UIViewController ,PHPickerViewControllerDeleg
     @objc func didTouchUpInsideAddPhotoButton(_ sender: Any) {
         var configuration = PHPickerConfiguration()
         configuration.filter = .any(of: [.images, .videos])
+        configuration.selectionLimit = 10 - selectedPhotos.count
 
         let picker = PHPickerViewController(configuration: configuration)
 
@@ -34,35 +37,35 @@ class SelectedPhotoViewController: UIViewController ,PHPickerViewControllerDeleg
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true)
         
-        let itemProvider = results.first?.itemProvider
-        if let itemProvider = itemProvider,
-           itemProvider.canLoadObject(ofClass: UIImage.self) {
+        itemProviders = results.map(\.itemProvider)
+        iterator = itemProviders.makeIterator()
+        
+        while let itemProvider = iterator?.next(),itemProvider.canLoadObject(ofClass: UIImage.self) {
             itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
                 DispatchQueue.main.async {
                     self.selectedPhotos.append(image as! UIImage)
                     self.collectionView.reloadData()
                 }
             }
-        } else {
-            // TODO: Handle empty results or item provider not being able load UIImage
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("list count",selectedPhotos.count + 1)
+        if(selectedPhotos.count >= 10){
+            return 10
+        }
         return selectedPhotos.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print(indexPath.row == (selectedPhotos.count) )
-        if indexPath.row == (selectedPhotos.count) {
-            print("Last")
+        if indexPath.row == (selectedPhotos.count), selectedPhotos.count < 10 {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AddButtonCell", for: indexPath) as! AddButtonCollectionViewCell
             cell.addPhotoButton.addTarget(self, action: #selector(didTouchUpInsideAddPhotoButton(_:)), for: .touchUpInside)
+            cell.sizeToFit()
+            
             
             return cell
         }
-        print(selectedPhotos[indexPath.row])
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedPhotoCell", for: indexPath) as! SelectedPhotoCollectionViewCell
         
         cell.imageView.image = selectedPhotos[indexPath.row]
@@ -80,4 +83,25 @@ class SelectedPhotoViewController: UIViewController ,PHPickerViewControllerDeleg
     }
     */
 
+}
+
+extension SelectedPhotoViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if selectedPhotos.count == 0 {
+            return UIEdgeInsets(top: 0, left: (collectionView.frame.size.width-180)/2, bottom: 0, right: (collectionView.frame.size.width-180)/2)
+        }
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.row == selectedPhotos.count {
+            return CGSize(width: 180, height: 330)
+        }
+        return CGSize(width: 330, height: 330)
+    }
 }
