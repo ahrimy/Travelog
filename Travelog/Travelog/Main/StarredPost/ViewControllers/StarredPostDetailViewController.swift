84c6a185b7ce245580a3bfaee3d5302980ad695f
KangYena <62419355+KangYena@users.noreply.github.com>
2021-05-06 02:50:04 +0900

    feat : 북마크 포스트 상세 페이지 댓글창 연결 chore : smile 에셋 추가, 좋아요 버튼 이미지 수정

--- Travelog/Travelog/Main/StarredPost/ViewControllers/StarredPostDetailViewController.swift
+++ Travelog/Travelog/Main/StarredPost/ViewControllers/StarredPostDetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class StarredPostDetailViewController: UIViewController {
     
     // MARK: - IBOutlets
+    @IBOutlet weak var likesButton: UIButton!
+    @IBOutlet weak var likesCount: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
@@ -17,12 +19,36 @@ class StarredPostDetailViewController: UIViewController {
     
     // MARK: - Properties
     var data: StarredList?
+//    var likesCountInt: Int = 0
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.layer.cornerRadius = 45
     }
+    
+    @IBAction func tappedLikeButton(_ sender: Any){
+        if (likesButton.isSelected == false) {
+            likesButton.isSelected = true
+            likesButton.setImage(UIImage(named: "smile.fill"), for: .normal)
+//            likesButton.tintColor = .white
+//            likesCountInt += 1
+        }
+    
+//        else {
+//            likesButton.isSelected = false
+//            likesButton.setImage(UIImage(named: "smile"), for: .normal)
+//            likesButton.tintColor = .white
+//            likesCountInt -= 1
+//        }
+    }
+    
+//    @IBAction func tappedCommentField(_ sender: Any){
+//        guard let commentView = self.storyboard?.instantiateViewController(withIdentifier: "commentView") as? CommentViewController else {
+//            return
+//        }
+//        self.present(commentView, animated: true, completion: nil)
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -35,6 +61,7 @@ class StarredPostDetailViewController: UIViewController {
         textLabel.text = data?.text
         imageView.image = UIImage(named: data?.image ?? "")
         
+//        likesCount.text = "\(likesCountInt)"
         
         locationLabel.font = UIFont.systemFont(ofSize: 17)
         locationLabel.numberOfLines = 1
