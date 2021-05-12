41f7b5a5cc907f082541bd93a9e8115dfb1f6ccb
ahrimy <dlsdjdkfla@gmail.com>
2021-05-13 01:43:28 +0900

    fix: mypost list 이미지 사이즈 수정

--- Travelog/Travelog/Main/MyPost/ViewControllers/MyPostListViewController.swift
+++ Travelog/Travelog/Main/MyPost/ViewControllers/MyPostListViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class MyPostListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class MyPostListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     // MARK: - Properties
     var posts:[PostOverview] = []
 //    var posts:[PostThumbnail] = []
-    let cellIdentifier: String = "cell"
+//    let cellIdentifier: String = "cell"
     
     // MARK: - IBOutlet
     @IBOutlet weak var collectionView: UICollectionView!
@@ -22,15 +22,15 @@ class MyPostListViewController: UIViewController, UICollectionViewDataSource, UI
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let flowLayout: UICollectionViewFlowLayout
-        flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets.zero
-        flowLayout.minimumInteritemSpacing = 5
-        flowLayout.minimumLineSpacing = 5
-        
-        flowLayout.estimatedItemSize = CGSize(width: 125, height: 125)
-        
-        self.collectionView.collectionViewLayout = flowLayout
+//        let flowLayout: UICollectionViewFlowLayout
+//        flowLayout = UICollectionViewFlowLayout()
+//        flowLayout.sectionInset = UIEdgeInsets.zero
+//        flowLayout.minimumInteritemSpacing = 5
+//        flowLayout.minimumLineSpacing = 5
+//
+//        flowLayout.estimatedItemSize = CGSize(width:  collectionView.frame.width/3, height:  collectionView.frame.width/3)
+//
+//        self.collectionView.collectionViewLayout = flowLayout
     }
 
     // MARK: - Methods
@@ -48,12 +48,32 @@ class MyPostListViewController: UIViewController, UICollectionViewDataSource, UI
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyPostViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyPostOverviewCell", for: indexPath) as! MyPostViewCell
         
         cell.overview = posts[indexPath.row]
         cell.imageView.image = posts[indexPath.row].image
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let width = collectionView.frame.width
+            let itemsPerRow: CGFloat = 3
+            let cellWidth = width / itemsPerRow
+
+            return CGSize(width: cellWidth, height: cellWidth)
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+      return  UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+      return 0
     }
 
     /*
