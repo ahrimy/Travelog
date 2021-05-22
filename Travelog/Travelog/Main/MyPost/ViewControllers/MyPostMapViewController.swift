acd8108a78a873af0c2f0b8db25469c4df8ae427
ahrimy <dlsdjdkfla@gmail.com>
2021-05-23 01:14:41 +0900

    feat: 지도 Pin clustering 기능 추가

--- Travelog/Travelog/Main/MyPost/ViewControllers/MyPostMapViewController.swift
+++ Travelog/Travelog/Main/MyPost/ViewControllers/MyPostMapViewController.swift
@@ -8,10 +8,9 @@
 import UIKit
 import MapKit
 
-class MyPostMapViewController: UIViewController,CLLocationManagerDelegate {
+class MyPostMapViewController: UIViewController {
     
     // MARK: - Properties
-    var locationManager:CLLocationManager!
     var posts:[PostOverview] = []
     
     // MARK: - IBOutlet
@@ -22,7 +21,7 @@ class MyPostMapViewController: UIViewController,CLLocationManagerDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        setup()
+        configureMapView()
     }
     
 
@@ -36,7 +35,7 @@ class MyPostMapViewController: UIViewController,CLLocationManagerDelegate {
     }
     */
     // MARK: - Methods
-    private func setup(){
+    private func configureMapView(){
         mapView.translatesAutoresizingMaskIntoConstraints = false
         
         mapView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
@@ -44,78 +43,50 @@ class MyPostMapViewController: UIViewController,CLLocationManagerDelegate {
         mapView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
-        setLocationManager()
         mapView.delegate = self
-        mapView.showsUserLocation = true
-    }
-    private func setLocationManager() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        mapView.register(
+            MapItemAnnotationView.self,
+            forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        mapView.register(
+            ClusterAnnotationView.self,
+            forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
     }
     func appendPost(post:PostOverview){
         self.posts.append(post)
-        self.addMapPin(with: post.coordinate, title: post.id, image: post.image)
+        self.addAnnotation(with: post)
     }
     func loadPosts(posts: [PostOverview]) {
-        self.posts = posts
+        self.posts = posts.sorted(by: {$0.createdAt < $1.createdAt})
         self.posts.forEach{post in
-            self.addMapPin(with: post.coordinate, title: post.id, image:post.image)
+            self.addAnnotation(with: post)
+        }
+        if let recentPost = self.posts.last {
+            setRegion(coordinate: recentPost.coordinate.coordinate)
         }
     }
     
-    func addMapPin(with location: CLLocation, title:String, image :UIImage){
-        // TODO : Delta 값 수정 필요
-        mapView.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 3.0, longitudeDelta: 3.0)), animated: true)
-        mapView.addAnnotation(PostAnnotation(coordinate: location.coordinate, title:title, image: image))
+    private func addAnnotation(with post: PostOverview){
+        mapView.addAnnotation(MapItem(post: post))
+    }
+    private func setRegion(coordinate: CLLocationCoordinate2D){
+        let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 10.0, longitudeDelta: 10.0))
+        mapView.setRegion(region, animated: true)
     }
 
 }
 
 // MARK: - map
 extension MyPostMapViewController: MKMapViewDelegate {
-    // annotation이 만들어 질 때 실행
-    public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if !(annotation is PostAnnotation){
-            return nil
-        }
-        
-        let postAnnotation = annotation as! PostAnnotation
-//        print("위치 정보 = \(annotation.coordinate)")
-        return registerImageInMapView(mapView: mapView, annotation: annotation, imgParam: postAnnotation.image)
-    }
-    
-    private func registerImageInMapView(mapView: MKMapView, annotation: MKAnnotation, imgParam: UIImage) -> MKAnnotationView? {
-        if annotation is MKUserLocation {
-            return nil
-        } else {
-            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "reuseId") ?? MKAnnotationView()
-            let width = self.mapView.frame.width / 7
-            annotationView.annotation = annotation
-            
-            let backgroundView = UIView(frame: CGRect(x: 0, y: 0, width: width + 10, height: width + 10 ))
-            backgroundView.backgroundColor = UIColor.white
-            backgroundView.layer.cornerRadius = 5
-            
-            let imageView = UIImageView(image: imgParam)
-            imageView.layer.masksToBounds = true
-            imageView.layer.cornerRadius = 5
 
-            backgroundView.addSubview(imageView)
-            annotationView.addSubview(backgroundView)
-            imageView.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                imageView.widthAnchor.constraint(equalTo: backgroundView.widthAnchor, multiplier: 0.92),
-                imageView.heightAnchor.constraint(equalTo: backgroundView.heightAnchor, multiplier: 0.92),
-                imageView.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
-                imageView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
-            ])
-            
-            return annotationView
-        }
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard view is ClusterAnnotationView else { return }
         
+        // if the user taps a cluster, zoom in
+        let currentSpan = mapView.region.span
+        let zoomSpan = MKCoordinateSpan(latitudeDelta: currentSpan.longitudeDelta/10.0, longitudeDelta: currentSpan.longitudeDelta/10.0)
+        let zoomCoordinate = view.annotation?.coordinate ?? mapView.region.center
+        let zoomed = MKCoordinateRegion(center: zoomCoordinate, span: zoomSpan)
+        mapView.setRegion(zoomed, animated: true)
     }
+
 }
