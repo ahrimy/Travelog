08d4cbfd81c4fe53dd78356aa79fee85dd86910f
KangYena <62419355+KangYena@users.noreply.github.com>
2021-05-31 03:17:56 +0900

    feat :  북마크 상세 페이지 이미지뷰 여러 개 나오도록 함, page control 추가 (single page이면 hide 되도록 수정해야함)

---
+++ Travelog/Travelog/Main/StarredPost/ViewControllers/StarredPostDetailImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  StarredPostDetailImageCell.swift
+//  Travelog
+//
+//  Created by 강예나 on 2021/05/31.
+//
+
+import UIKit
+
+class StarredPostDetailImageCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+}
