0121665e64c16fd08dd9664a382631f1b7677eb2
ahrimy <dlsdjdkfla@gmail.com>
2021-06-09 02:49:48 +0900

    chore: Attraction Post List Cell 파일 추가

---
+++ Travelog/Travelog/Main/AttractionPostCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AttractionPostCollectionViewCell.swift
+//  Travelog
+//
+//  Created by Ahrim Yang on 2021/06/09.
+//
+
+import UIKit
+
+class AttractionPostCollectionViewCell: UICollectionViewCell {
+    
+}
